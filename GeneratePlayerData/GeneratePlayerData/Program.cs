3bcb2e6ac994de24ea34d9b7702660f825a19097
Taylor Jensen <Taylor Jensen>
2016-11-25 19:29:46 -0800

    Defense is working now, exe now takes 2 optional parameters. First parameter is true/false for including thursday games. 2nd parm is path/file to use for altering projections

--- GeneratePlayerData/GeneratePlayerData/Program.cs
+++ GeneratePlayerData/GeneratePlayerData/Program.cs
@@ -19,19 +19,25 @@ namespace GeneratePlayerData
 
         static void Main(string[] args)
         {
+            bool includeThursdayGames = args.Length > 0 ? Convert.ToBoolean(args[0]) : true;
+            string diffFilePath = args.Length > 1 ? args[1] : "";
             Task.Run(async () =>
             {
-                await GatherAndGeneratePlayerFile();
+                await GatherAndGeneratePlayerFile(includeThursdayGames, diffFilePath);
             }).Wait();
         }
 
-        public static async Task GatherAndGeneratePlayerFile()
+        public static async Task GatherAndGeneratePlayerFile(bool includeThursdayGames, string diffFilePath)
         {
             var yahooPlayerData = FetchYahooPlayerData();
             var projectionsData = FetchNumberfireProjectionsData();
 
             await Task.WhenAll(yahooPlayerData, projectionsData);
-            await MergeDataAndOutputResults(yahooPlayerData.Result, projectionsData.Result);
+            await MergeDataAndOutputResults(
+                yahooPlayerData.Result, 
+                projectionsData.Result, 
+                includeThursdayGames,
+                diffFilePath);
         }
 
         public static async Task<IEnumerable<YahooPlayerData>> FetchYahooPlayerData()
@@ -40,23 +46,40 @@ namespace GeneratePlayerData
             var response = await client.GetAsync(YAHOO_PLAYER_DATA_URI);
             var result = await response.Content.ReadAsStringAsync();
             var jsonResult = JsonConvert.DeserializeObject<YahooResponse>(result);
+            var toReturn = jsonResult.Result.Players;
+            foreach(var player in toReturn)
+            {
+                player.Name = NormalizeName(player.Name);
+            }
             return jsonResult.Result.Players;
         }
 
         public static async Task<IEnumerable<NumberfirePlayerData>> FetchNumberfireProjectionsData()
         {
             HttpClient client = new HttpClient();
-            var response = await client.GetAsync(PROJECTIONS_DATA_URI);
-            var result = await response.Content.ReadAsStringAsync();
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(result);
+            var playerResponse = await client.GetAsync(PROJECTIONS_DATA_URI);
+            var playerResult = await playerResponse.Content.ReadAsStringAsync();
+            var defenseResponse = await client.GetAsync(PROJECTIONS_DATA_URI + "/D");
+            var defenseResult = await defenseResponse.Content.ReadAsStringAsync();
+            HtmlDocument playerDoc = new HtmlDocument();
+            playerDoc.LoadHtml(playerResult);
+            HtmlDocument defenseDoc = new HtmlDocument();
+            defenseDoc.LoadHtml(defenseResult);
+
             // Find the two correct tables, both are the only tbodys with children on the page.
-            IEnumerable<HtmlNode> tableNodes = doc.DocumentNode.SelectNodes("//tbody").Where(n => n.ChildNodes.Count > 1);
+            IEnumerable<HtmlNode> tableNodes = playerDoc.DocumentNode.SelectNodes("//tbody").Where(n => n.ChildNodes.Count > 1);
+            IEnumerable<HtmlNode> defenseTableNodes = defenseDoc.DocumentNode.SelectNodes("//tbody").Where(n => n.ChildNodes.Count > 1);
 
             // Iterate through the two tables constructing the player objects.
             var playerInfo = tableNodes.ElementAt(0).Descendants("tr");
             var projectionsInfo = tableNodes.ElementAt(1).Descendants("tr");
             var zippedInfo = playerInfo.Zip(projectionsInfo, (info, proj) => new { info, proj});
+
+            var defenseInfo = defenseTableNodes.ElementAt(0).Descendants("tr");
+            var defenseProjectionInfo = defenseTableNodes.ElementAt(1).Descendants("tr");
+            var zippedDefenseInfo = defenseInfo.Zip(defenseProjectionInfo, (info, proj) => new { info, proj });
+            zippedInfo = zippedInfo.Concat(zippedDefenseInfo);
+
             List<NumberfirePlayerData> playerData = new List<NumberfirePlayerData>();
             foreach(var playerRow in zippedInfo)
             {
@@ -65,17 +88,21 @@ namespace GeneratePlayerData
                 {
                     throw new Exception("Data Player IDs unaligned.");
                 }
-                string playerName = playerRow.info.Descendants("a")
-                    .Where(a => a.Attributes["class"].Value.Contains("full")).First().InnerText.Trim();
-                string position = playerRow.info.Descendants("span")
-                    .Where(a => a.Attributes["class"].Value.Contains("player-info--position")).First().InnerText.Trim();
-                string projection = playerRow.proj.Descendants("td")
-                    .Where(a => a.Attributes["class"].Value.Contains("fp")).First().InnerText.Trim();
+                var playerName = playerRow.info.Descendants("a")
+                    .Where(a => a.Attributes["class"].Value.Contains("full")).FirstOrDefault();
+                var position = playerRow.info.Descendants("span")
+                    .Where(a => a.Attributes["class"].Value.Contains("player-info--position")).FirstOrDefault();
+                var projection = playerRow.proj.Descendants("td")
+                    .Where(a => a.Attributes["class"].Value.Contains("fp")).FirstOrDefault();
+                var playerNameText = playerName != null ? playerName.InnerText.Trim() : "";
+                var positionText = position != null ? position.InnerText.Trim() : "def";
+                var projectionText = projection != null ? projection.InnerText.Trim() : "0";
+
                 playerData.Add(new NumberfirePlayerData()
                 {
-                    Name = playerName,
-                    Projection = Convert.ToDecimal(projection),
-                    Position = position
+                    Name = NormalizeName(playerNameText),
+                    Projection = Convert.ToDecimal(projectionText),
+                    Position = positionText
                 });
             }
             return playerData;
@@ -83,8 +110,28 @@ namespace GeneratePlayerData
 
         public static async Task MergeDataAndOutputResults(
             IEnumerable<YahooPlayerData> yahooPlayerData,
-            IEnumerable<NumberfirePlayerData> numberfireData)
+            IEnumerable<NumberfirePlayerData> numberfireData,
+            bool includeThursdayPlayers,
+            string diffFilePath)
         {
+            List<PlayerProjectionAlteration> projectionAlterations = new List<PlayerProjectionAlteration>();
+            if (!string.IsNullOrEmpty(diffFilePath))
+            {
+                using (var reader = new StreamReader(diffFilePath))
+                {
+                    string line;
+                    while((line = reader.ReadLine()) != null)
+                    {
+                        var projAlterItems = line.Split(',');
+                        projectionAlterations.Add(new PlayerProjectionAlteration()
+                        {
+                            Name = NormalizeName(projAlterItems[0]),
+                            Difference = Convert.ToDecimal(projAlterItems[1])
+                        });
+                    }
+                }
+            }
+
             using (FileStream fs = File.Open("mergedPlayers.csv", FileMode.Create))
             {
                 using (var writer = new StreamWriter(fs))
@@ -96,12 +143,21 @@ namespace GeneratePlayerData
                             n => n.Name == player.Name && 
                             (!string.IsNullOrEmpty(n.Position) ? 
                             n.PositionEnum == player.PositionEnum : true)).FirstOrDefault();
-                        if (matchingPlayer != null)
+
+                        var exclude = !includeThursdayPlayers && player.GameStartTime.DayOfWeek == DayOfWeek.Thursday;
+                        if (matchingPlayer != null && !exclude)
                         {
+                            var projectedPoints = matchingPlayer.Projection;
+                            var projectionAlteration = projectionAlterations.Where(pa => pa.Name == pa.Name).FirstOrDefault();
+                            if (projectionAlteration != null)
+                            {
+                                projectedPoints += projectionAlteration.Difference;
+                            }
+
                             var output = string.Format("{0},{1},{2},{3},",
-                                NormalizeName(player.Name),
+                                player.Name,
                                 player.Salary,
-                                matchingPlayer.Projection,
+                                projectedPoints,
                                 (int)player.PositionEnum);
                             writer.WriteLine(output);
                         }
@@ -115,6 +171,47 @@ namespace GeneratePlayerData
             Regex rgx = new Regex("[^a-z ]+");
             name = rgx.Replace(name.ToLower(), "");
             name = name.Replace(" sr", "").Replace(" jr", "");
+            // Check if the name is a defense/city player
+            string[] dsts = {
+                "Baltimore",
+                "Houston",
+                "Arizona",
+                "Denver",
+                "Carolina",
+                "Kansas City",
+                "Chicago",
+                "Jacksonville",
+                "New England",
+                "New Orleans",
+                "Tampa Bay",
+                "Los Angeles",
+                "Philadelphia",
+                "New York Jets",
+                "Dallas",
+                "Cincinnati",
+                "Cleveland",
+                "San Diego",
+                "Minnesota",
+                "Washington",
+                "San Francisco",
+                "Atlanta",
+                "New York Giants",
+                "Miami",
+                "Green Bay",
+                "Seattle",
+                "Tennessee",
+                "Pittsburgh",
+                "Oakland",
+                "Detroit",
+                "Indianapolis",
+                "Buffalo"
+            };
+            foreach(var d in dsts)
+            {
+                if (name.Contains(d.ToLower()))
+                    name = d.ToLower();
+            }
+
             return name;
         }
     }
