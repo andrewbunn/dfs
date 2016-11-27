ab081acd63541b84ca5fd8e31275839891b9e56f
Taylor Jensen <Taylor Jensen>
2016-11-26 18:06:51 -0800

    Added new project for PlayerDataAggregation. This project will (eventually) output a file with results for each player each week with injury information. Aggregation part is done, just need to output to a file

---
+++ PlayerDataAggregator/PlayerDataAggregator/PlayerResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerDataAggregator
+{
+    public class PlayerResult
+    {
+        public PlayerResult()
+        {
+            Scores = new List<float>();
+            Injuries = new List<bool>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public List<bool> Injuries { get; set; }
+        public List<float> Scores { get; set; }
+    }
+}
